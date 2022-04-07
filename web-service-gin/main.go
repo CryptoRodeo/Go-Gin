2a28eec940336c90c7e38c0e98811b2f4ab089e3
Bryan Ramos <bramos@redhat.com>
2022-04-07 14:29:04 -0400

    refactor

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,33 +1,20 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"example/web-service-gin/album"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
-
-    router.Run("localhost:8080")
-}
-// album represents data about a record album.
-type album struct {
-    ID string `json:"id"`
-    Title string `json:"title"`
-    Artist string `json:"artist"`
-    Price float64 `json:"price"`
+	router := gin.Default()
+	createRoutes(router)
+	router.Run("localhost:8080")
 }
 
-// albums slice to seed record album data
+func createRoutes(router *gin.Engine) {
+	router.GET("/albums", album.GetAlbums)
+	router.GET("/albums/:id", album.GetAlbumByID)
 
-var albums = []album {
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-//getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
+	router.POST("/albums", album.PostAlbums)
 }
