39e4d0a51fad9b0e0aa49fb8d2def99d2379dc12
Bryan Ramos <bramos@redhat.com>
2022-04-07 14:36:38 -0400

    refactor main package

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"example/web-service-gin/album"
+	"example/web-service-gin/routing"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	createRoutes(router)
+	routing.CreateRoutes(router)
 	router.Run("localhost:8080")
-}
-
-func createRoutes(router *gin.Engine) {
-	router.GET("/albums", album.GetAlbums)
-	router.GET("/albums/:id", album.GetAlbumByID)
-
-	router.POST("/albums", album.PostAlbums)
 }
